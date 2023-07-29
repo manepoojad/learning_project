a5425a5feda2c16ed1b1dbefc38b5f9c25c2236e
Pooja Mane <manepoojad@gmail.com>
2023-07-29 17:43:52 +0530

    second commit

--- src/pages/users/Users.js
+++ src/pages/users/Users.js
@@ -1,81 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import './users.css'
+import React from 'react'
 
 function Users() {
-  const navigate = useNavigate()
-  const [productList, setProductList] = useState([])
-
-  useEffect(() => {
-    getProductList()
-  }, [])
-
-  const getProductList = async () => {
-
-    const response = await fetch('https://fakestoreapi.com/products', {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    const responseData = await response.json()
-    // console.log(responseData)
-    setProductList(responseData)
-  }
-  // console.log(productList)
-
-  const handleEditProductData = (item) => {
-    navigate(`/editProduct/${item.id}`, {
-      state: {
-        productData: { ...item }
-      }
-    })
-  }
-
   return (
-    <div className='productTableWrapper'>
-      <table>
-        <thead>
-          <tr>
-            <th style={{width:"20px"}}>S.N</th>
-            <th style={{width:"20px"}}>ID</th>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Category</th>
-            <th>Image</th>
-            <th>Price</th>
-            <th style={{width:"40px"}}>Count</th>
-            <th style={{width:"30px"}}>Rate</th>
-            <th style={{width:"40px"}}>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            productList && productList.map((item, index) => {
-              {/* console.log(item) */}
-              {
-                return (
-                  <tr key={index}>
-                    <td>{index + 1}</td>
-                    <td>{item.id}</td>
-                    <td>{item.title}</td>
-                    <td>{item.description}</td>
-                    <td>{item.category}</td>
-                    <td><img src={item.image} height={50} width={50}/></td>
-                    <td>{item.price}</td>
-                    <td>{item.rating.count}</td>
-                    <td>{item.rating.rate}</td>
-                    <td>
-                      <button type="button" onClick={() => handleEditProductData(item)}>Edit</button>
-                    </td>
-                  </tr>
-                )
-              }
-            })
-          }
-        </tbody>
-      </table>
-      </div>
+    <div>Users</div>
   )
 }
 
